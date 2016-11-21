7bed32393534a9fd0a4b2358f91e0529e6a47a60
xiaoxu <baixu1217@163.com>
2016-11-21 19:09:39 +0800

    循环引用问题

--- shared_ptr.cpp
+++ shared_ptr.cpp
@@ -1,91 +1,31 @@
 #include<iostream>
 using namespace std;
-//运用引用计数的方式
+#include<memory>
+#include<boost/shared_ptr.hpp>
 template<typename T>
-class SharedPtr
+class Node
 {
 public:
-	SharedPtr(T* p = NULL)
-		:_p(p)
-		,_pCount(new int(1))
-	{
-		cout<<"SharedPtr(T* p = NULL)"<<endl;
-	}
-	SharedPtr(SharedPtr& sp)
-		:_p(sp._p)
-		,_pCount(sp._pCount)
-	{
-		cout<<"SharedPtr(SharedPtr& sp)"<<endl;
-	}
-	SharedPtr<T>& operator=(const SharedPtr& sp)
-	{
-		cout<<"SharedPtr<T>& operator=(const SharedPtr& sp)"<<endl;
-		if(_p != sp._p)
-		{
-			if(_p == NULL)
-			{
-				_p = sp._p;
-				_pCount = sp._pCount;
-			}
-			else if(_p && (*_pCount== 1))
-			{
-				delete _p;
-				_p = sp._p;
-				_pCount = sp._p;
-			}
-			else
-			{
-				--(*_pCount);
-				_p = sp._p;
-				_pCount = sp._pCount;
-			}
-			if(sp._p)
-				++(*_pCount);
-		}
-		
-		return *this;
-	}
-	~SharedPtr()
-	{
-		cout<<"~SharedPtr()"<<endl;
-		if(_p && --(*_pCount) == 0)
-		{
-			delete _p;
-			_p = NULL;
-		}
-	}
-public:
-	T& operator*()
-	{
-		return *_p;
-	}
-	T* operator->()
-	{
-		return _p;
-	}
-
-	int UseCount()
-	{
-		if(_pCount != NULL)
-			return *_pCount;
-		return 0;
-	}
-
-private:
-	T *_p;
-	int *_pCount;
+	Node(const T& value)
+		:_pNext(NULL)
+		,_pPre(NULL)
+		,_value(value)
+	{}
+	T _value;	
+	shared_ptr<Node<T>> _pNext;
+	shared_ptr<Node<T>> _pPre;
 };
 
 void FunTest()
 {
-	SharedPtr<int> sp(new int(1));
-	SharedPtr<int> sp1(sp);
-	SharedPtr<int> sp2;
-	sp = sp1;
-	sp2 = sp1;
-	cout<<sp.UseCount()<<endl;
-	cout<<sp1.UseCount()<<endl;
-	cout<<sp2.UseCount()<<endl;
+	shared_ptr<Node<int>> sp1(new Node<int>(1));
+	shared_ptr<Node<int>> sp2(new Node<int>(2));
+	cout<<sp1.use_count()<<endl;
+	cout<<sp2.use_count()<<endl;
+	sp1->_pNext = sp2;
+	sp2->_pPre = sp1;
+	cout<<sp1.use_count()<<endl;
+	cout<<sp2.use_count()<<endl;
 }
 
 int main()
