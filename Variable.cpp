c48c97b16b176ba3541edfb891d5d44adbfbb191
xiaoxu <baixu1217@163.com>
2016-12-13 13:06:37 +0800

    可变参数列表的应用

---
+++ Variable.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+#include<stdarg.h>
+
+int Sum(int val,...)
+{
+	va_list va;
+	va_start(va,val);
+	int sum = 0;
+	for(size_t i = 0; i < val; ++i)
+	{
+		sum += va_arg(va,int);
+	}
+	return sum;
+}
+
+int main()
+{
+	cout<<Sum(2,3,4)<<endl;
+    return 0;
+}
