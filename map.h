40aeb934433a50e76840c9249e7f7a3d71509c1a
baixu <baixu1217@163.com>
2017-06-09 17:21:31 +0800

    添加了set的实现

--- map.h
+++ map.h
@@ -4,10 +4,10 @@
 using namespace std;
 #include<map>
 #include<string>
-
-void TestMap()
-{
-	map<string,int> countMap;
+#include<set>
+//void TestMap()
+//{
+	//map<string,int> countMap;
 	/*string strs[] = {"苹果","香蕉","橘子","苹果","苹果","香蕉"};*/
 
 	//1.从头到尾遍历看是否有数组中的值，没有则插入，有则使second++
@@ -74,7 +74,73 @@ void TestMap()
 	//	it++;
 	//}
 
+	/*typedef map<string,string> Dict;
+	typedef map<string,string>::iterator DictIt;*/
+	//typedef multimap<string,string> Dict;
+	//typedef multimap<string,string>::iterator DictIt;
+	//Dict dict;
+	//dict.insert(make_pair("left","左边"));
+	//dict.insert(make_pair("right","右边"));
+	//dict.insert(make_pair("left","剩余"));
+
+
+	//DictIt it = dict.begin();
+
+	//it = dict.find("left");
+	//dict.erase(it);
+	//it = dict.begin();
+	//while (it != dict.end())
+	//{
+	//	cout<<it->first<<":"<<it->second<<endl;
+	//	++it;
+	//}
+
+
+//}
 
 
 
+void TestSet()
+{
+	/*typedef set<string> MySet;
+	typedef set<string>::iterator MySetIt;*/
+
+	/*MySet myset;
+	string strs[] = {"苹果","香蕉","橘子","西瓜","草莓","樱桃"};
+
+	for(int i = 0; i < sizeof(strs)/sizeof(strs[0]); i++)
+	{
+		myset.insert(strs[i]);
+	}
+
+	MySetIt it = myset.begin();
+	if(it == myset.find("哈密瓜"))
+			cout<<"哈密瓜存在"<<endl;
+		else
+			cout<<"哈密瓜不存在"<<endl;
+	cout<<"存在的其他水果为："<<endl;
+	it = myset.begin();
+	while(it != myset.end())
+	{
+		cout<<*it<<endl;
+		
+		++it;
+	}*/
+
+	/*typedef set<int> MySet;
+	typedef set<int>::iterator MySetIt;
+*/
+	typedef multiset<int> MySet;
+	typedef multiset<int>::iterator MySetIt;
+	MySet myset;
+	for(int i = 10; i > 0; i--)
+		myset.insert(i);
+	
+	myset.insert(5);
+	MySetIt it = myset.begin();
+	while(it != myset.end())
+	{
+		cout<<*it<<endl;
+		++it;
+	}
 }
